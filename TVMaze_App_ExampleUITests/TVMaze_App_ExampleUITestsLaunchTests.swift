33c85d27b8cab70e6646f3310aeb24a2f1bc5c3d
José Antonio Caballero Martínez <ja.caballero.mtz@gmail.com>
2024-03-19 17:22:03 -0600

    Initial Commit

---
+++ TVMaze_App_ExampleUITests/TVMaze_App_ExampleUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  TVMaze_App_ExampleUITestsLaunchTests.swift
+//  TVMaze_App_ExampleUITests
+//
+//  Created by José Caballero on 19/03/24.
+//
+
+import XCTest
+
+final class TVMaze_App_ExampleUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
